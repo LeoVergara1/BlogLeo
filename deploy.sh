c10475e87b0e2c37dc6a9a1506df18474f49afe2
LeoVergara1 <leovergara.dark@gmail.com>
2017-01-18 09:54:47 -0600

    prueba

--- deploy.sh
+++ deploy.sh
@@ -21,4 +21,5 @@ git add -A
 git commit -m "$comm"
 git push origin master --force
 echo "8.- Actualizado repositorio de makingdevs.github.io, revisa cambios en el blog ----------------------------"
+
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
